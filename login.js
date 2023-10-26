215f95d1ec3fb575d08824a8ec3a36caaa67e158
jrp244 <83414799+jrp244@users.noreply.github.com>
2023-10-25 22:44:16 -0600

    added some more js 3

--- login.js
+++ login.js
@@ -1,16 +1,32 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    // You can implement actual authentication logic here
-    // For simplicity, we'll just display a message
-    displayMessage(`Welcome, ${username}!`);
-  });
-  
-  function displayMessage(message) {
-    const outputContent = document.getElementById('outputContent');
-    outputContent.innerHTML = `<p>${message}</p>`;
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // You can implement actual authentication logic here
+  // For simplicity, we'll just display a message
+  displayMessage(`Welcome, ${username}!`);
+});
+
+function displayMessage(message) {
+  const outputContent = document.getElementById('outputContent');
+  outputContent.innerHTML = `<p>${message}</p>`;
+}
+
+// Function to handle the demo login
+document.getElementById('demoLoginButton').addEventListener('click', () => {
+  // Simulate a demo login
+  const demoUsername = 'demoUser';
+  const demoPassword = 'demoPassword';
+
+  // Check if the demo login credentials are correct
+  if (
+    document.getElementById('username').value === demoUsername &&
+    document.getElementById('password').value === demoPassword
+  ) {
+    displayMessage('Demo Login Successful');
+  } else {
+    displayMessage('Demo Login Failed. Please use the correct credentials.');
   }
-  
+});
