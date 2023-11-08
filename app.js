36c2125e29040d20cad1c07d8a85b00a9269d38a
jrp244 <83414799+jrp244@users.noreply.github.com>
2023-11-08 15:24:43 -0700

    added node.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000; // You can change the port as needed
+
+// Serve static files (HTML, CSS, JavaScript)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/scores', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'scores.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
