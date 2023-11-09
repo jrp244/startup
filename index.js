3e71c210528253fc4a0d61c4d87c72e2fa429370
jrp244 <83414799+jrp244@users.noreply.github.com>
2023-11-08 18:46:25 -0700

    renamed index.js and added package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000; // You can change the port as needed
+const port = process.env.PORT || 4000; // You can change the port as needed
 
 // Serve static files (HTML, CSS, JavaScript)
 app.use(express.static(path.join(__dirname, 'public')));
