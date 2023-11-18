d3fdb58b44908779911b25290584e5189b579c98
jrp244 <83414799+jrp244@users.noreply.github.com>
2023-11-17 18:57:32 -0700

    login.js stuff added

--- public/login.js
+++ public/login.js
@@ -1,20 +1,35 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+// login.js
+
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
   event.preventDefault();
 
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
-  // You can implement actual authentication logic here
-  // For simplicity, we'll just display a message
-  displayMessage(`Welcome, ${username}!`);
+  try {
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      displayMessage(`Welcome, user with ID ${result.id}!`);
+      document.getElementById('loginForm').reset();
+    } else {
+      const result = await response.json();
+      displayMessage(`Authentication failed: ${result.msg}`);
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+    displayMessage('An error occurred during login.');
+  }
 });
 
-function displayMessage(message) {
-  const outputContent = document.getElementById('outputContent');
-  outputContent.innerHTML = `<p>${message}</p>`;
-}
-
-// Function to handle the demo login
+// Function to handle the demo login (similar to before)
 document.getElementById('demoLoginButton').addEventListener('click', () => {
   // Simulate a demo login
   const demoUsername = 'demoUser';
@@ -26,7 +41,13 @@ document.getElementById('demoLoginButton').addEventListener('click', () => {
     document.getElementById('password').value === demoPassword
   ) {
     displayMessage('Demo Login Successful');
+    document.getElementById('loginForm').reset();
   } else {
     displayMessage('Demo Login Failed. Please use the correct credentials.');
   }
 });
+
+function displayMessage(message) {
+  const outputContent = document.getElementById('outputContent');
+  outputContent.innerHTML = `<p>${message}</p>`;
+}
